dd103f21889a8ef4a64d2f86f881d67a2a222039
Jaime Deng <jaime@Jaimes-MacBook-Pro.local>
2020-08-21 21:37:34 -0700

    online on discord

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.once("ready",() => {
+    console.log("islapMusic is online")
+})
+
+// last line
+client.login("NzQ2NTg1ODE4Njg0MDYzODM1.X0CeOg.N-Y55SzTBBJU1wx5-KiwWOQjCDo");
